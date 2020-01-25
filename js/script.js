9a72ea36666b5215df5314149b316bc9477e97c6
musmangeee <musmangeee@bitbucket.org>
2020-01-25 19:18:05 +0500

    New updates.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    // $(window).resize(function (e) {
+    //     var parentwidth = $(".intervention-box").width();
+    //     $('.video1').each(function () {
+    //         parentwidth = $(this).width();
+    //     });
+    //     $(".side-panel-inner").toggleClass("fixed").width(parentwidth);
+    // });
+
+})
